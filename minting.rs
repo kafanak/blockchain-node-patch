9bbbae6d625cd7027a955cdba3abec7bdfe0905b
kafanak <Kafanaka@protonmail.com>
2023-03-14 14:00:01 +0200

    Improve smart contract logging

--- minting.rs
+++ minting.rs
@@ -1,3 +1,4 @@
 // Add support for decentralized lending platforms
 // Integrate with external DeFi protocols
 // Add native support for staking rewards
+// Refactor contract to use EIP-712 signatures
