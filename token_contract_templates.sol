d1833b625fb3c1770717e227c4fa7d143fa0f799
kafanak <Kafanaka@protonmail.com>
2023-03-20 14:00:01 +0200

    Fix bugs in multi-token systems

--- token_contract_templates.sol
+++ token_contract_templates.sol
@@ -1,3 +1,4 @@
 // Integrate with decentralized identity providers
 // Enhance governance contract flexibility
 // Improve gas estimation for transactions
+// Refactor contract to use EIP-712 signatures
