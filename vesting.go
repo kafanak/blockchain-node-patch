e5470964cab1369c67cd1d8611589d8b2bad83ca
kafanak <Kafanaka@protonmail.com>
2023-05-29 14:00:00 +0200

    Fix issues in multi-token smart contracts

--- vesting.go
+++ vesting.go
@@ -1,3 +1,4 @@
 // Improve gas estimation for transactions
 // Fix errors in contract interaction with wallets
 // Improve security for token minting contracts
+// Add support for decentralized file storage
