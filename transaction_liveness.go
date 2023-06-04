4cd5dbc435d874915587bd2e551457e1f0d1d76e
kafanak <Kafanaka@protonmail.com>
2023-06-04 14:00:01 +0200

    Enhance contract event handling

--- transaction_liveness.go
+++ transaction_liveness.go
@@ -1,3 +1,4 @@
 // Fix governance proposal approval logic
 // Optimize gas usage for contract deployment
 // Improve error handling in transaction flow
+// Add support for batch transaction processing
