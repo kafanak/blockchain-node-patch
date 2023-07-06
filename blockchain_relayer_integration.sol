56736623eff5b23f1617cf63a39898cd34e4e391
kafanak <Kafanaka@protonmail.com>
2023-07-06 14:00:01 +0200

    Improve DApp transaction flow

--- blockchain_relayer_integration.sol
+++ blockchain_relayer_integration.sol
@@ -1,3 +1,4 @@
 // Fix security vulnerability in random number generation
 // Fix issues with gas optimization in smart contracts
 // Refactor contract to use EIP-712 signatures
+// Implement on-chain identity verification
