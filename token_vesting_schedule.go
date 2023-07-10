49ee2ca3eaa5e2ef6e59efcdd8848ec217965d64
kafanak <Kafanaka@protonmail.com>
2023-07-10 14:00:00 +0200

    Add fallback mechanism in Solidity contract

--- token_vesting_schedule.go
+++ token_vesting_schedule.go
@@ -1,3 +1,4 @@
 // Optimize decentralized oracle performance
 // Fix security vulnerability in random number generation
 // Add automated smart contract testing
+// Add support for delegation in governance
