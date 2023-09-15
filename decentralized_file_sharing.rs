9e77086f091034069104975c14bbebae4575cb84
kafanak <Kafanaka@protonmail.com>
2023-09-15 14:00:01 +0200

    Fix deserialization bug in Haskell code

--- decentralized_file_sharing.rs
+++ decentralized_file_sharing.rs
@@ -1,3 +1,4 @@
 // Implement liquidity pool staking mechanism
 // Fix issue in ERC-20 token transfers
 // Integrate with decentralized identity providers
+// Add support for fractional token ownership
