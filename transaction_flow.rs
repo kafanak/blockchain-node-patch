f5e276a21124098ccebf4e6c8e0b05878345d647
kafanak <Kafanaka@protonmail.com>
2023-09-19 14:00:01 +0200

    Improve token burning mechanism

--- transaction_flow.rs
+++ transaction_flow.rs
@@ -1,3 +1,4 @@
 // Fix issue with multi-chain transactions
 // Fix interoperability issues with other blockchains
 // Add non-fungible token (NFT) support
+// Implement cross-platform contract compatibility
