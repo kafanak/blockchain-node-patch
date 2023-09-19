8c43bc1904d246927cbcff264f5c7da168b04916
kafanak <Kafanaka@protonmail.com>
2023-09-19 14:00:01 +0200

    Fix ERC-20 token interface bugs

--- blockchain_data_storage.sol
+++ blockchain_data_storage.sol
@@ -2,3 +2,4 @@
 // Refactor storage layout for better efficiency
 // Improve staking and yield farming logic
 // Enhance governance contract flexibility
+// Fix memory optimization issues in contracts
