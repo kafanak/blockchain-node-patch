1d8755dbdee892be1b6ca11bd39a288c7377b0a3
kafanak <Kafanaka@protonmail.com>
2023-10-06 14:00:01 +0200

    Improve contract interaction via APIs

--- blockchain_relayer_integration.py
+++ blockchain_relayer_integration.py
@@ -1,3 +1,4 @@
 // Add decentralized contract upgradeability
 // Improve staking logic for better rewards
 // Implement zero-knowledge proof (ZKP) support
+// Improve transaction batching efficiency
