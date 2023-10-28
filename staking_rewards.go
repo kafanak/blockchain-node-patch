47e9faf9f1afb7663ba6981c0432a22a70b89da9
kafanak <Kafanaka@protonmail.com>
2023-10-28 14:00:00 +0200

    Enhance smart contract audit tools

--- staking_rewards.go
+++ staking_rewards.go
@@ -1,3 +1,4 @@
 // Add governance proposal expiration feature
 // Enhance test coverage for Rust module
 // Add support for multiple consensus mechanisms
+// Fix issues in multi-token smart contracts
