a23db2e11c5436dd960eafe01300a895a9056334
kafanak <Kafanaka@protonmail.com>
2023-11-13 14:00:00 +0200

    Add governance proposal expiration feature

--- api_rate_limiting.rs
+++ api_rate_limiting.rs
@@ -1,3 +1,4 @@
 // Fix errors in contract interaction with wallets
 // Add support for multiple consensus mechanisms
 // Fix ERC-721 contract issues with transfers
+// Add decentralized contract upgradeability
