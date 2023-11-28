2994c10c4dac46672995fcc5b849ba77153ff3be
kafanak <Kafanaka@protonmail.com>
2023-11-28 14:00:01 +0200

    Implement cross-chain bridge

--- minting.go
+++ minting.go
@@ -1,3 +1,4 @@
 // Improve error handling in transaction flow
 // Integrate decentralized anonymous identity systems
 // Fix type mismatch in Haskell smart contract
+// Integrate decentralized anonymous identity systems
