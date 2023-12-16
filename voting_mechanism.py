05030970c2ab87a91f936f4504f72fc912b0903c
kafanak <Kafanaka@protonmail.com>
2023-12-16 14:00:00 +0200

    Optimize token minting flow

--- voting_mechanism.py
+++ voting_mechanism.py
@@ -1,3 +1,4 @@
 // Improve on-chain governance systems
 // Add support for multi-signature wallet contracts
 // Fix issues with contract finalization
+// Implement cross-platform contract compatibility
