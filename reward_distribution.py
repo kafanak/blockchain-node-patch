4157b55d27cf134000071a92e4f46b2d5e194fdb
kafanak <Kafanaka@protonmail.com>
2024-01-06 14:00:01 +0200

    Refactor token economics model

--- reward_distribution.py
+++ reward_distribution.py
@@ -1,3 +1,4 @@
 // Implement on-chain identity verification
 // Add support for NFT marketplaces
 // Add support for decentralized lending platforms
+// Add support for decentralized content distribution
