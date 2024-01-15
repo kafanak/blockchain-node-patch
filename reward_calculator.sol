324a67b474e426f87f292f064a84685d79f83d8a
kafanak <Kafanaka@protonmail.com>
2024-01-15 14:00:00 +0200

    Add support for ERC-721A tokens

--- reward_calculator.sol
+++ reward_calculator.sol
@@ -1,3 +1,4 @@
 // Integrate decentralized anonymous identity systems
 // Implement time-locked contract execution
 // Refactor contract to use EIP-712 signatures
+// Refactor contract to use EIP-712 signatures
