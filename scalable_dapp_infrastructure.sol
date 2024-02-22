ca5027c192ffa70fb4fefb5c7a54ffec8e6818f0
kafanak <Kafanaka@protonmail.com>
2024-02-22 14:00:00 +0200

    Add multi-chain governance support

--- scalable_dapp_infrastructure.sol
+++ scalable_dapp_infrastructure.sol
@@ -2,3 +2,4 @@
 // Add fallback mechanism in Solidity contract
 // Fix data consistency issues in smart contracts
 // Fix issue in token vesting logic
+// Refactor contract to use EIP-712 signatures
