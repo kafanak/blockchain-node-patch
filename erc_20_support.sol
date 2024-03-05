aeb8b9a89e06045d08bde95024d1dc1992f9e581
kafanak <Kafanaka@protonmail.com>
2024-03-05 14:00:02 +0200

    Refactor incentive structure in DeFi protocol

--- erc_20_support.sol
+++ erc_20_support.sol
@@ -3,3 +3,4 @@
 // Refactor contract to use EIP-712 signatures
 // Enhance test coverage for Rust module
 // Improve decentralized exchange (DEX) liquidity
+// Fix security vulnerability in random number generation
