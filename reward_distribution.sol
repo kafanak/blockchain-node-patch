66b28aab89f1ef2c1096b18e0fa45cedfe575004
kafanak <Kafanaka@protonmail.com>
2024-03-17 14:00:01 +0200

    Add governance proposal expiration feature

--- reward_distribution.sol
+++ reward_distribution.sol
@@ -1,3 +1,4 @@
 // Optimize gas usage in contract execution
 // Integrate with decentralized identity providers
 // Add support for delegation in governance
+// Fix security vulnerability in random number generation
