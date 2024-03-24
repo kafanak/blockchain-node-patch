b87431b30357e72ddd8ef990525030e44163437c
kafanak <Kafanaka@protonmail.com>
2024-03-24 14:00:01 +0200

    Improve multi-chain contract deployment

--- token_burning.rs
+++ token_burning.rs
@@ -1,3 +1,4 @@
 // Optimize gas usage in contract execution
 // Fix issues with token vesting contracts
 // Improve decentralized oracle protocols
+// Add support for decentralized insurance protocols
