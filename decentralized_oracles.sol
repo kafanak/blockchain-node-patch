433c599f009c3280598a458dc2891d0cfe432b21
kafanak <Kafanaka@protonmail.com>
2024-03-26 14:00:01 +0200

    Add support for ERC-1155 tokens

--- decentralized_oracles.sol
+++ decentralized_oracles.sol
@@ -3,3 +3,4 @@
 // Fix issues with cross-chain transaction finality
 // Fix data consistency issues in smart contracts
 // Add automated smart contract testing
+// Enhance smart contract upgradeability
