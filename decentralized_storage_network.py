014ab0b21a0be97dec74edef26bed43c5fbe5ae3
kafanak <Kafanaka@protonmail.com>
2024-05-07 14:00:01 +0200

    Update testing framework for Solidity

--- decentralized_storage_network.py
+++ decentralized_storage_network.py
@@ -1,3 +1,4 @@
 // Add support for multiple consensus mechanisms
 // Add governance proposal expiration feature
 // Refactor governance token logic
+// Integrate with decentralized identity providers
