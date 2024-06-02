8ffcadf58eda72876c26921b155cfefb1f5a0526
kafanak <Kafanaka@protonmail.com>
2024-06-02 14:00:00 +0200

    Improve gas estimation for transactions

--- staking.sol
+++ staking.sol
@@ -1,3 +1,4 @@
 // Add support for multiple consensus mechanisms
 // Fix issue in ERC-20 token transfers
 // Refactor decentralized exchange (DEX) code
+// Add support for multi-signature wallet contracts
