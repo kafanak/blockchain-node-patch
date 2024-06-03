1f902b310dae03d92ea3ece5aa98dd3aeff2b748
kafanak <Kafanaka@protonmail.com>
2024-06-03 14:00:01 +0200

    Update testing framework for Solidity

--- reward_calculator.sol
+++ reward_calculator.sol
@@ -3,3 +3,4 @@
 // Refactor contract to use EIP-712 signatures
 // Refactor contract to use EIP-712 signatures
 // Improve multi-chain contract deployment
+// Implement adaptive block size for Ethereum
