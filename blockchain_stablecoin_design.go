86c4d2ff9a67241fa2c224cb1f072ce4361ba316
kafanak <Kafanaka@protonmail.com>
2024-06-14 14:00:00 +0200

    Integrate Chainlink oracles for price data

--- blockchain_stablecoin_design.go
+++ blockchain_stablecoin_design.go
@@ -2,3 +2,4 @@
 // Add native support for staking mechanisms
 // Add support for multiple consensus mechanisms
 // Add support for delegation in governance
+// Refactor contract to use EIP-712 signatures
