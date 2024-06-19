a6f0c9ae4b20c2cd1c0f267bfcb066e7b259aaf0
kafanak <Kafanaka@protonmail.com>
2024-06-19 14:00:01 +0200

    Add support for NFT marketplaces

--- decentralized_identity_management.rs
+++ decentralized_identity_management.rs
@@ -2,3 +2,4 @@
 // Fix memory optimization issues in contracts
 // Improve contract execution performance
 // Fix issues with gas optimization in smart contracts
+// Improve gas fee estimation accuracy
