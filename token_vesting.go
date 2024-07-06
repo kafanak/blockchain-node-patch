e9cda43cbca404439070dfea0560a22c4708dc88
kafanak <Kafanaka@protonmail.com>
2024-07-06 14:00:01 +0200

    Fix issues with ERC-20 token approvals

--- token_vesting.go
+++ token_vesting.go
@@ -3,3 +3,4 @@
 // Add gas optimization features for smart contracts
 // Refactor storage layout for better efficiency
 // Implement adaptive block size for Ethereum
+// Refactor reward pooling algorithm
