3cb6de174ca829da14842a43a8efbb105d783525
kafanak <Kafanaka@protonmail.com>
2024-07-06 14:00:01 +0200

    Implement price feed oracles for tokens

--- token_standard_extensions.py
+++ token_standard_extensions.py
@@ -1,3 +1,4 @@
 // Fix governance proposal approval logic
 // Integrate privacy-enhancing smart contracts
 // Fix bugs in decentralized exchange routing
+// Add support for decentralized lending platforms
