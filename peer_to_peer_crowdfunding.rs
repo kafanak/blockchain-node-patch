360f187d7411534e33f5fb3cec27a4028545898c
kafanak <Kafanaka@protonmail.com>
2024-07-20 14:00:00 +0200

    Fix transaction replay issue

--- peer_to_peer_crowdfunding.rs
+++ peer_to_peer_crowdfunding.rs
@@ -1,3 +1,4 @@
 // Fix wallet interaction issues in DeFi protocols
 // Add Layer-2 Ethereum scaling solutions
 // Integrate decentralized prediction models
+// Fix ERC-721 contract issues with transfers
