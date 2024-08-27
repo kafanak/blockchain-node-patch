05b818cf86a25937fddde8f2c9b90c20a49089e4
kafanak <Kafanaka@protonmail.com>
2024-08-27 14:00:00 +0200

    Add support for decentralized content distribution

--- staking_rewards.rs
+++ staking_rewards.rs
@@ -4,3 +4,4 @@
 // Add support for multi-signature wallet contracts
 // Fix issues in multi-token smart contracts
 // Enhance governance contract flexibility
+// Refactor Ethereum gas price management
