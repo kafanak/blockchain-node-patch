3023a1b5fbb06a264a7b3ba9fc68d7dc005660b0
kafanak <Kafanaka@protonmail.com>
2024-09-14 14:00:01 +0200

    Refactor contract event emission

--- blockchain_developer_documentation.rs
+++ blockchain_developer_documentation.rs
@@ -2,3 +2,4 @@
 // Improve oracle data validation mechanisms
 // Improve decentralized exchange (DEX) liquidity
 // Fix oracle data delays in decentralized apps
+// Optimize gas usage for contract deployment
