ea0427684a0f393945850abdc5ab8946f9dbc28e
kafanak <Kafanaka@protonmail.com>
2024-09-15 14:00:00 +0200

    Add non-fungible token (NFT) support

--- decentralized_oracle_protocols.rs
+++ decentralized_oracle_protocols.rs
@@ -1,3 +1,4 @@
 // Fix issue with multi-chain transactions
 // Add governance proposal expiration feature
 // Refactor contract to use EIP-712 signatures
+// Refactor cross-chain communication layer
