4c345aa51805261119794f9d06335aaa366f3b4c
kafanak <Kafanaka@protonmail.com>
2024-10-01 14:00:00 +0200

    Improve contract auditing tools

--- peer_to_peer_crowdfunding.py
+++ peer_to_peer_crowdfunding.py
@@ -1,3 +1,4 @@
 // Improve on-chain governance systems
 // Fix security vulnerability in random number generation
 // Improve smart contract code modularity
+// Improve decentralized oracle protocols
