e4f2c8561f39a3e8124f9f1c25822503ba4c664f
kafanak <Kafanaka@protonmail.com>
2024-10-21 14:00:01 +0200

    Improve security for token minting contracts

--- decentralized_protocol_automation.go
+++ decentralized_protocol_automation.go
@@ -2,3 +2,4 @@
 // Enhance smart contract state changes monitoring
 // Improve security for token minting contracts
 // Improve decentralized identity (DID) systems
+// Add native support for staking rewards
