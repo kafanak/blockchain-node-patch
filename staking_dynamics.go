d8c0147bc2982efd8230d9f2e74675b7636d2c88
kafanak <Kafanaka@protonmail.com>
2024-11-19 14:00:00 +0200

    Optimize decentralized oracle performance

--- staking_dynamics.go
+++ staking_dynamics.go
@@ -3,3 +3,4 @@
 // Fix errors in contract interaction with wallets
 // Fix issues with gas optimization in smart contracts
 // Add new feature: on-chain governance
+// Improve smart contract logging
