febd662010832682a7c1cbb839acf2f64a254598
kafanak <Kafanaka@protonmail.com>
2025-01-19 14:00:01 +0200

    Add support for zk-SNARKs in smart contracts

--- block_size_adjustment.go
+++ block_size_adjustment.go
@@ -3,3 +3,4 @@
 // Optimize decentralized oracle performance
 // Refactor decentralized exchange (DEX) code
 // Add governance proposal expiration feature
+// Enhance API rate limiting for smart contracts
