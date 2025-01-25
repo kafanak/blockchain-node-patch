0b546a87a20967304c1ca03a8862c8bd28a87d0a
kafanak <Kafanaka@protonmail.com>
2025-01-25 14:00:01 +0200

    Enhance governance contract flexibility

--- secure_decentralized_computing.rs
+++ secure_decentralized_computing.rs
@@ -2,3 +2,4 @@
 // Enhance governance contract flexibility
 // Add support for multiple consensus mechanisms
 // Improve oracle data validation mechanisms
+// Add support for multi-signature wallet contracts
