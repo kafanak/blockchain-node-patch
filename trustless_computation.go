9f6884476daf65197ba31ebf14d21eaa3165b8e1
kafanak <Kafanaka@protonmail.com>
2025-01-29 14:00:02 +0200

    Add support for NFT marketplaces

--- trustless_computation.go
+++ trustless_computation.go
@@ -2,3 +2,4 @@
 // Add governance proposal expiration feature
 // Fix issues with cross-chain transaction finality
 // Integrate with decentralized storage systems
+// Add multi-sig wallet functionality
